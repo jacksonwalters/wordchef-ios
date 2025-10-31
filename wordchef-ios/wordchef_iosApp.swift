8d758e9d464d0a3138e3bd8b09d5b2b3d7b7c40c
Jackson Walters <jacksonwalters@gmail.com>
2025-10-31 12:29:17 -0400

    add initial Swift files

---
+++ wordchef-ios/wordchef_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordchef_iosApp.swift
+//  wordchef-ios
+//
+//  Created by Jackson Walters on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct wordchef_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wordchef-iosTests/wordchef_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  wordchef_iosTests.swift
+//  wordchef-iosTests
+//
+//  Created by Jackson Walters on 10/31/25.
+//
+
+import Testing
+@testable import wordchef_ios
+
+struct wordchef_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
